caa912ab27f6759fd2a1fdf22f11524930426b71
Gizex <49234898+Gizex@users.noreply.github.com>
2023-01-22 18:57:38 +0300

    Update notification_template.go
    fixed links to dash

--- notification_template.go
+++ notification_template.go
@@ -1,7 +1,8 @@
-{{ define "tgshortbody" }}{{ range . }} <b>{{ or .Labels.alertname }}</b>
-{{ range .Annotations.SortedPairs }}<b>{{ .Name }}:</b> {{ .Value }}
-{{ end }}
-{{ with .ValueString }}{{ reReplaceAll "[[][^]]*metric='{?([^}']*)}?'[^]]*value=([0-9]*([.][0-9]{,3})?)[^]]*](, )?" "<b>$1:</b> <code> $2 </code> \n" . }}
-{{ end }}{{ with .PanelURL }}<a href="{{ . }}">Panel</a>    {{ end }}{{ with .DashboardURL }}<a href="{{ . }}">Dashboard</a>{{ end }}{{ with .GeneratorURL }} <a href="{{ . }}"> </a> {{ end }}{{ with .SilenceURL }} <a href="{{ . }}"></a> {{ end }}
+{{ define "tgshortbody" }}{{ range . }} <b>#{{ or .Labels.alertname }}</b>
+{{ if gt (len .Annotations) 0 }}
+<b>Summary</b>: {{.Annotations.summary}}
+<b>{{.Annotations.description}}</b>{{ end }}{{ range .Labels.SortedPairs }}
+<b>{{ .Name | title }}</b>:  <code>{{ .Value }}</code>{{ end }}
+{{ with .PanelURL }}<a href="{{ . }}">Panel</a>    {{ end }}{{ with .DashboardURL }}<a href="{{ . }}">Dashboard</a>{{ end }}{{ with .GeneratorURL }} <a href="{{ . }}"> </a> {{ end }}{{ with .SilenceURL }} <a href="{{ . }}"></a> {{ end }}
 {{ end }}{{ end }}
 {{ define "tgshort" }}{{ with .Alerts.Firing }}🔥[Alerting]{{ template "tgshortbody" . }}{{ end }}{{ with .Alerts.Resolved }}✅[ОК]{{ template "tgshortbody" . }}{{ end }}{{ end }}
